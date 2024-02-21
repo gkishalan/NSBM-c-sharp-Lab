5cc39169de88929b5360abe800668ac916a47c95
Kishalan <Kishalan@LAPTOP-UBOSMR80>
2024-02-21 11:33:06 +0530

    Add project files.

---
+++ Example 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Write A program to dispaly your name and age*/
+            //Console.WriteLine("Name : Kishalan");
+            //Console.WriteLine("Age : 20");
+
+            /*Write a name as a input*/
+            //Console.WriteLine("What is your name?");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Hello " + name);
+            //Console.ReadLine();
+
+            
+    }
+}
